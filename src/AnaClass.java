5107900985fc255ad703953b9f3d7371f5ccb198
ecemtellal <91099163+ecemtellal@users.noreply.github.com>
2024-04-03 01:04:34 +0300

    first commit

--- src/AnaClass.java
+++ src/AnaClass.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class AnaClass {
+import java.util.Scanner;
+public class AnaClass {
+    public static void main(String[] args) {
+        double  kdvsizTutar,kdvTutar, kdvOrani= 0.18, kdvliTutar;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Bir değer giriniz: ");
+        kdvsizTutar= input.nextDouble();
+
+        kdvOrani=(0 < kdvsizTutar && kdvsizTutar <=1000 )? 0.18:  0.08;
+
+        kdvTutar=kdvOrani*kdvsizTutar;
+        kdvliTutar= kdvsizTutar + kdvTutar;
+
+        System.out.println("KDV'siz Tutar:" +kdvsizTutar);
+        System.out.println("KDV Tutar:" +kdvTutar);
+        System.out.println("KDV'Lİ Tutar:" +kdvliTutar);
+        System.out.println("KDV Oranı:" +kdvOrani);
+
+
+    }
 }
